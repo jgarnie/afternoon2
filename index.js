8676aa63c7b866f17d4d2756af78081a7d317d5d
jgarnie <garcia.nieto.joaquin@gmail.com>
2020-05-19 21:25:40 +0200

    jajaj

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-'use strict'
 
+  class item {
+    constructor(name, price, quantity) {
+      this.name = name;
+      this.price = price;
+      this.quantity = quantity;
+    }
+    //this is the function i am trying to activate
+    sell = () => {
+      if (this.quantity > 0) {
+        this.quantity--;
+      }
+      return `${this.name} ${this.price} ${this.quantity-1}`; //actually now you dont need this
+    };
+
+    store = (count) => {
+      return `${this.name} ${this.price} ${this.quantity+count}`;
+    }
+  }
+  const items = [
+    new item('soap', 11, 3),
+    new item('bread', 12, 2),
+    new item('apples', 13, 5),
+    new item('banana', 15, 7),
+    new item('grappe', 16, 5),
+    new item('water', 9, 7),
+    new item('beer', 4, 9),
+  ];
+
+  const renderItems = (items) => {
+
+    const list = document.querySelector('.elements-body');
+    list.innerHTML = '';
+
+    for (let i = 0; i < items.length; i++) {
+      const newLi = document.createElement('tr');
+      newLi.innerHTML += `<td>${items[i].name}</td><td>${items[i].price}</td><td>${items[i].quantity}</td><button class="buyer">Buy</button>`;
+      list.appendChild(newLi);
+      let btnBuy = newLi.querySelector('.buyer');
+      //this is the listener that should activate sell(),
+      btnBuy.addEventListener('click', (e) => {
+        e.preventDefault();
+        items[i].sell();
+        renderItems(items);
+      });
+    }
+  };
+
+  renderItems(items);
+
+
+
+
+/*
+/*
 class item{
 
     constructor(name, price, quantity)
@@ -11,8 +64,8 @@ class item{
 
     }
 
-    sell(){
-        
+    sell(e){
+        e.preventDefault;
         return `${this.name} ${this.price} ${this.quantity-1}`
        
     }
@@ -49,10 +102,10 @@ for(let i=0; i<items.length; i++){
     let btnBuy = newLi.querySelector('.buyer');
     
 
-    btnBuy.addEventListener('click', item.sell);
+    btnBuy.addEventListener('click', sell);
+   
 
-
-}
+}*/
 /*items.forEach((item) => {
     const list = document.querySelector('.elements');
 
